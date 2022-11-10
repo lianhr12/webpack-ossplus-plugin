8702e7d16fe256277cc0c15b8d5aeb3d222d622c
hrope <lhr9311@gmail.com>
2022-11-10 19:24:06 +0800

    build: 构建配置调整

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
 import { terser } from 'rollup-plugin-terser';
+// 支持require引入模块
+import commonjs from '@rollup/plugin-commonjs';
+// 默认情况下rollup.js不支持导入json模块
+import json from '@rollup/plugin-json'
 
 export default [
   // 编译TS文件为UMD模块标准
@@ -12,6 +16,10 @@ export default [
       name: 'webpackossplusplugin'
     },
     plugins: [
+      json(),
+      commonjs({
+        namedExports: {'tslib': ['__awaiter', '__generator']}
+     }),
       typescript(),
       terser()
     ]
@@ -24,6 +32,10 @@ export default [
       format: "cjs",
     },
     plugins: [
+      json(),
+      commonjs({
+        namedExports: {'tslib': ['__awaiter', '__generator']}
+     }),
       typescript()
     ]
   },
@@ -35,6 +47,10 @@ export default [
       format: "es" //这里的es指的就是将源码编译成esmodule规范的文件
     },
     plugins: [
+      json(),
+      commonjs({
+        namedExports: {'tslib': ['__awaiter', '__generator']}
+     }),
       typescript(),
     ]
   },
